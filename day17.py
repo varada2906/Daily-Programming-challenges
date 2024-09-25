8b7207c4bae39b526d4eed0c132b1aa281ddacb3
Varada Korlahalli <varadakorlahalli@gmail.com>
2024-09-25 21:19:51 +0530

    mm

---
+++ day17.py
@@ -0,0 +1,24 @@
+import math
+def primeFactors(n):
+    # Print the number of two's that divide n
+    while n % 2 == 0:
+        print (2,end=' ')
+        n = n / 2
+        
+#for odd
+    for i in range(3, int(math.sqrt(n))+1, 2):
+        
+        
+        while n % i == 0:
+            print (i,end=' ')
+            n = n / i
+            
+   #for number greater than 2
+    if n > 2:
+        print (n,end=' ')
+        
+
+n = 18
+primeFactors(n)
+
+
